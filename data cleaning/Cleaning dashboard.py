c988dc197125140ca70cdfc6cc643afe56ac1a1e
Joris Lesterhuis <J.lesterhuis@student.tue.nl>
2024-05-05 14:14:57 +0200

    Made a dashboard to simplify the data cleaning for others

---
+++ data cleaning/Cleaning dashboard.py
@@ -0,0 +1,20 @@
+import data_cleans
+
+# Make sure that all of your data is in one data folder
+# The data folder must be at the same level (have same parent folder) as the folder this dashboard is in 
+
+# Fill in the name of your data folder here:
+data_folder_name: str = '*ENTER NAME OF DATA FOLDER HERE*'
+
+# Fill in the name of the parent folder of this file
+data_cleaning_folder_name: str = 'data cleaning' # 'data cleaning' by default
+
+# Once folder_name contains the correct name
+# Run this file (Will take a while)
+# Once it has finished running you should see a print message in your terminal
+# The new file with the cleaned data will appear in the same folder as the data as 'airline_data'
+
+
+# DO NOT TOUCH THIS
+path: str = f'{data_cleaning_folder_name}/../{data_folder_name}'
+data_cleans.clean_all_files(path)
