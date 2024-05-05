e83a4e8a1d7cd4535271ca2dfe6be8edb4ec336b
esteerutten <e.rutten@student.tue.nl>
2024-05-05 11:29:09 +0200

    database_connect

--- database/database_connect.py
+++ database/database_connect.py
@@ -1,12 +1,12 @@
 import pymongo
-import subprocess
+import json
+from pymongo import MongoClient, InsertOne
 import os
 
 #directory containing JSON files to import
 directory = "/Users/esteerutten/DataScienceFiles/DBL Data Challenge Map/data airlines"
 
 #connection details
-
 mongo_uri = "mongodb://localhost:27017/"
 database_name ="DBL2" #name of the database you wanna connect to 
 
@@ -18,13 +18,3 @@ database = client[database_name]
 collection = database["mycollection"] #insert name of the collection
 collection.insert_one({"key": "value"})
 
-# MongoDB Compass CLI command for importing JSON files
-compass_command = 'mongodb-compass import --file {} --collection {} --uri "{}{}"'
-
-# Iterate over JSON files in the directory
-for filename in os.listdir(directory):
-    if filename.endswith('.json'):
-        filepath = os.path.join(directory, filename)
-        collection_name = os.path.splitext(filename)[0]  # Use file name as collection name
-        command = f'mongoimport --uri "{mongo_uri}" --db {database_name} --collection {collection_name} --file {filepath} --jsonArray'
-        subprocess.run(command, shell=True)
