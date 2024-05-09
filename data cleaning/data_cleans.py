33c93d0424712fb64ea00e034c37ddb68a15fbd7
Joris Lesterhuis <J.lesterhuis@student.tue.nl>
2024-05-09 12:43:17 +0200

    Cleaned up data cleaning: - Moved redundant code into 'Leftover code' file - Merged 'data_cleans' and 'new_cleans' into one file - Added missing docstrings and comments

--- data cleaning/data_cleans.py
+++ data cleaning/data_cleans.py
@@ -1,33 +1,12 @@
-from os import read
-import re 
-import fileinput
-from typing import Iterator
 import os
 import json
-import new_cleans
-
-def old_remove_variables(tweet: dict) -> dict:
-    '''
-    Takes a tweet as a string and removes unnecessary variables
-    :param tweet: string representation of tweet
-    :returns: a string containing the cleaned version of the tweet
-    '''
-
-    variables_to_remove: list[str] = ["source", "location", "url", "protected", "utc_offset", "time_zone", "geo_enabled", "contributors_enabled", "is_translator", "profile_background_color", "profile_background_image_url", "profile_background_image_url_https", "profile_background_tile", "profile_link_color", "profile_sidebar_border_color", "profile_sidebar_fill_color", "profile_text_color", "profile_use_background_image", "profile_image_url", "profile_image_url_https", "profile_banner_url", "default_profile", "default_profile_image", "following", "follow_request_sent", "notifications"]
-
-    # Removes the following variables from tweet string:
-    for variable in variables_to_remove:
-       del tweet[variable]
-
-    return tweet
-
 
 
 def make_tweet_list(path: str)-> list[dict]: 
     """
     Reads the data from the file located at path
     :param path: path to the data to be loaded
-    :returns: list with a tweets
+    :returns: list of tweets
     """
     print('cleaning:'+ path)
    
@@ -44,116 +23,95 @@ def make_tweet_list(path: str)-> list[dict]:
     
 
 
-def check_tweet(tweet: dict) -> bool:
-    """
-    Checks whether the tweet should be kept by looking at media and language
-    :param tweet: string representation of tweet
-    :returns: A boolean value representing wether we want to keep the tweet in the data
-    """
-    if tweet["lang"] == "en":
+def file_paths_list(path_to_data_folder: str) -> list[str]:
+    '''
+    Generates a list which contains the paths to all files in the given data folder
+    :param path_to_data_folder: a string path to the data folder that we want to access
+    :returns: a list of string paths to all individual files in the data folder
+    '''
 
-        for key in tweet:
-            if type(tweet[key]) == dict:
-                check_tweet(tweet[key])
-        # Check whether the tweet has media
-        if 'media' in tweet:
-            return False
-        # Check whether the tweet is in english
-        elif tweet["lang"] == "en":
-            return True
-        else:
-            return False
-    
-
-
-
-def clean_file(old_path: str, new_path:str) -> None:
-    """
-    Replaces the json file from old_path with a cleaned version of the file at new_path
-    :param old_path: the path to the file that is to be cleaned
-    :param new_path: the path where the new cleaned file will be generated
-    """
-    # Get each individual line from the file
-    with open(old_path, 'r') as file:
-       lines = file.readlines()
-
-    # Write the cleaned version of the lines into the new file
-    with open(new_path, 'a') as new_file:
-        for number, line in enumerate(lines):
-
-            # Checks if the tweet should be kept and if so adds it to the new file
-            if check_tweet(line) == True:
-                new_file.write(remove_variables(line))
-            else:
-                new_file.write('')
-    
-
-
-def make_file_iterator(path: str) -> Iterator:
-    """
-    Creates and returns an iterator that contains each line of a file
-    :param path: the path to the file
-    :returns: an iterator of each line in the file found at path
-    """
-    # Get each individual line from the file
-    return fileinput.input(path)
-
-
-
-
-
-def file_paths_list(path_to_data_folder: str) -> list:
     file_path_list: list = list()
     file_names_list: list = os.listdir(path_to_data_folder)
+
     for file_name in file_names_list:
-        file_path_list.append(f'{path_to_data_folder}/{file_name}')  
+        file_path_list.append(f'{path_to_data_folder}/{file_name}')
+      
     return file_path_list
 
 
 
-def check_file(path: str) -> None:
+def remove_variables(tweet: dict) -> dict:
+    '''
+    Cleans a given tweet by removing unnecessary variables.
+    :param tweet: a dictionary representing a tweet
+    :returns: a cleaned version of the tweet as a dictionary
+    '''
     
-    
-    # Initialize a list for keeping track of duplicates  
-    lines = make_tweet_list(path)
-    
-    with open(f'{path}/../tweet_variables', 'a') as new_file:
-            # Checks if the tweet should be kept and if so adds it to the new file
-            new_file.write('') 
-    
-    # go through all lines
-    for tweet in lines:
+    # All of the keys we want to clean
+    variables_to_remove: list[str] = ["source", "location", "url", "protected", "utc_offset", "time_zone", "geo_enabled", "contributors_enabled", "is_translator", "profile_background_color", "profile_background_image_url", "profile_background_image_url_https", "profile_background_tile", "profile_link_color", "profile_sidebar_border_color", "profile_sidebar_fill_color", "profile_text_color", "profile_use_background_image", "profile_image_url", "profile_image_url_https", "profile_banner_url", "default_profile", "default_profile_image", "following", "follow_request_sent", "notifications"]
 
-        if check_tweet(tweet):
-            # check if we've seen a duplicate of it before
-            current_tweet_variables: list[str] = get_tweet_variables(tweet)
+    keys_to_remove = list()
 
-            if current_tweet_variables[0] == 'None' or current_tweet_variables[1] == 'None' or current_tweet_variables[2] == 'None':
-                continue
+    for key in tweet:
+        # Recurse through each tweet in the tweet object
+        if type(tweet[key]) == dict:
+            remove_variables(tweet[key])
 
-            if current_tweet_variables not in make_tweet_list(f'{path}/../tweet_variables'):
-                # if it's new, add it to the list we've seen
-                store_tweet_variables(current_tweet_variables, path)
+        # Note all keys in the tweet that are also in the variables to remove list
+        if key in variables_to_remove:                
+            keys_to_remove.append(key)
 
-    return None
+    # Remove each key
+    for key in keys_to_remove:
+        del tweet[key]
+    return tweet
 
-def store_tweet_variables(variables: list[str] ,path: str) -> None:
-    with open(f'{path}/../tweet_variables', 'a') as new_file:
-            # Checks if the tweet should be kept and if so adds it to the new file
-            new_file.write(str(variables)+'\n') 
 
-def get_tweet_variables(tweet: str) -> list[str]:
 
-    text_regex = '\"text\":\"[^\"]+\",'
-    user_name_regex = '\"name\":[^,]+,'
-    timestamp_ms_regex = '\"timestamp_ms\":.+'
+def check_language(tweet: dict) -> bool:
+    '''
+    Checks whether a tweet is in english
+    :param tweet: the tweet to be checked
+    :returns: a boolean value representing whether the tweet is english
+    '''
 
-    current_tweet_variables: list[str] = [str(re.search(text_regex, tweet)), str(re.search(user_name_regex, tweet))]
+    if 'lang' in tweet:
+        return tweet['lang'] == 'en'
+    else:
+        return False
 
-    if len(re.findall(timestamp_ms_regex, tweet)) > 0:
-        current_tweet_variables.append(str(re.findall(timestamp_ms_regex, tweet)[-1]))
-    
-    return current_tweet_variables
+
+
+def check_media(tweet: dict) -> bool:
+    '''
+    Checks whether a tweet or any tweets that are inside the tweet contain any media elements
+    :param tweet: the tweet to be checked
+    :returns: a boolean value representing whether the tweet has any media content
+    '''
+    # Check the main tweet
+    if 'media' in tweet:
+        return True
+
+    # Check all tweets inside the tweet
+    for key in tweet:
+        if type(tweet[key]) == dict:
+            if check_media(tweet[key]):
+                return True
+
+    return False
+
+
+
+def check_delete(tweet: dict) -> bool:
+    '''
+    Checks whether the tweet is deleted or not.
+    :param tweet: the tweet to be checked
+    :returns: a boolean value representing whether the tweet has been deleted
+    '''
+
+    if 'delete' in tweet:
+        return True
+    return False
 
 
 
@@ -162,15 +120,20 @@ def clean_all_files(path: str) -> None:
     Cleans all data and adds it to a big file
     :param path: path to the data folder
     '''
+
     file_path_list = file_paths_list(path)
-    counter = 0
+    progress_counter = 0
+
     with open(f'{path}/cleaned_data.json', 'a') as new_file:
         for file_path in file_path_list:
-            counter += 1
-            print(str(counter) + '/' + str(len(file_path_list)))
+            # Keep track of how many files have been processed
+            progress_counter += 1
+            print(str(progress_counter) + '/' + str(len(file_path_list)))
+
+            # For each line/tweet in the file, check if it should be included and write it into the cleaned data.
             lines: list = make_tweet_list(file_path)
             for line in lines:
-                if new_cleans.check_language(line) and not new_cleans.check_media(line) and not new_cleans.check_delete(line):
-                    #new_file.write(remove_variables(line))
-                    new_file.write(json.dumps(new_cleans.remove_variables(line)) + '\n')
+                if check_language(line) and not check_media(line) and not check_delete(line):
+                    new_file.write(json.dumps(remove_variables(line)) + '\n')
+
     print(f"All files cleaned and inserted into {path}/cleaned_data.json")
