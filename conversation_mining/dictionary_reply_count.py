62b749b10fe7571af4c240ae3de3a7ce6c7037f0
esteerutten <e.rutten@student.tue.nl>
2024-05-23 18:54:12 +0200

    main branch to estee branch

--- conversation_mining/dictionary_reply_count.py
+++ conversation_mining/dictionary_reply_count.py
@@ -1,46 +1,3 @@
-# from pymongo import MongoClient
-
-# #fetching tweets
-
-# def fetch_tweet_ids(collection): 
-#     '''
-#     retrieves all tweet IDs from MongoDB
-#     ''' 
-#     cursor = collection.find({}, {'id': 1}) 
-#     tweet_ids = [document['id']for document in cursor]
-#     return tweet_ids
-
-# def insert_removed_duplicates(tweet_ids, collection): 
-#     '''
-#     insert removed duplicates into another MongoDB collection
-#     '''
-#     removed_duplicates = [{'id': tweet_id, 'reply_count': 0} for tweet_id in tweet_ids]
-#     collection.insert_many(removed_duplicates)
-
-# def increment_reply_count(collection, tweet_id, increment_by=1): 
-#     '''
-#     increment reply count for a given tweet_id
-#     '''
-#     collection.update_one({'id': tweet_id}, {'$inc': {'reply_count': increment_by}})
-
-# if __name__ == '__main__': 
-#     client = MongoClient('mongodb://localhost:27017/')
-
-#     db = client['AirplaneMode'] # your database
-#     tweets_collection = db['removed_duplicates'] #existing collection
-
-#     # db = client['AirplaneMode'] # Myungwon's database
-#     # tweets_collection = db['removed_duplicates'] # Myungwon's existing collection
-
-#     tweet_ids = fetch_tweet_ids(tweets_collection)
-
-#     dict_count_replies = db['count_replies']
-#     insert_removed_duplicates(tweet_ids, dict_count_replies)
-
-#     # #practice test
-#     # for doc in dict_count_replies.find().limit(50):
-#     #     print(doc)
-
 from pymongo import MongoClient, ASCENDING
 
 def connect_to_db():
@@ -51,22 +8,6 @@ def connect_to_db():
     # Select the collection
     collection = db['removed_duplicates']
     return db, collection
-
-def remove_duplicates(collection):
-    # Aggregate to find duplicate id_str
-    pipeline = [
-        {"$group": {"_id": "$id_str", "count": {"$sum": 1}, "ids": {"$push": "$_id"}}},
-        {"$match": {"count": {"$gt": 1}}}
-    ]
-    
-    duplicates = list(collection.aggregate(pipeline))
-    
-    for duplicate in duplicates:
-        # Keep the first occurrence and remove the rest
-        ids_to_remove = duplicate['ids'][1:]
-        collection.delete_many({"_id": {"$in": ids_to_remove}})
-        
-    return len(duplicates)
 
 def create_index_on_id_str(collection):
     # Create an index on the 'id_str' field
@@ -103,7 +44,7 @@ def create_id_str_dict(collection):
 
 def store_data_in_new_collection(db, data):
     # Create a new collection
-    new_collection = db['id_str_count']
+    new_collection = db['id_str_count_test']
     # Convert the dictionary to a list of documents
     documents = [{'id_str': key, 'count': value} for key, value in data.items()]
     # Insert the documents into the new collection
@@ -111,8 +52,6 @@ def store_data_in_new_collection(db, data):
 
 def main():
     db, collection = connect_to_db()
-    duplicate_count = remove_duplicates(collection)
-    print(f"Removed {duplicate_count} duplicate(s).")
     create_index_on_id_str(collection)
     id_str_dict = create_id_str_dict(collection)
     store_data_in_new_collection(db, id_str_dict)
