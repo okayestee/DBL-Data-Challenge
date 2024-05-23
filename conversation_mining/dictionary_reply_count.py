282dc07ba026e1362d5c4b1d1c6cc253c5b8dfa2
20305050 <m.kim@student.tue.nl>
2024-05-23 12:35:11 +0200

    update convo

--- conversation_mining/dictionary_reply_count.py
+++ conversation_mining/dictionary_reply_count.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient, ASCENDING
+from pymongo import MongoClient
 
 def connect_to_db():
     # Connect to the MongoDB server (modify the URI as needed)
@@ -9,53 +9,33 @@ def connect_to_db():
     collection = db['removed_duplicates']
     return db, collection
 
-def create_index_on_id_str(collection):
-    # Create an index on the 'id_str' field
-    index_name = collection.create_index([('id_str', ASCENDING)], unique=True)
-    return index_name
-
 def create_id_str_dict(collection):
     # Initialize an empty dictionary
     id_str_dict = {}
-    # Process documents in batches to avoid the 16MB limit
-    last_id = None
-    batch_size = 10000
 
-    while True:
-        # Query to get a batch of documents
-        if last_id:
-            query = {'_id': {'$gt': last_id}}
-        else:
-            query = {}
+    # Query to retrieve documents with id_str and in_reply_to_user_id_str fields
+    query = {'id_str': {'$exists': True}, 'in_reply_to_user_id_str': {'$exists': True}}
+    projection = {'id_str': 1, 'in_reply_to_user_id_str': 1, '_id': 0}
 
-        batch = list(collection.find(query).sort('_id', ASCENDING).limit(batch_size))
-        
-        if not batch:
-            break
-
-        for document in batch:
-            id_str = document.get('id_str')
-            if id_str and id_str not in id_str_dict:
-                id_str_dict[id_str] = 0
-
-        last_id = batch[-1]['_id']
+    # Iterate over documents and populate the dictionary
+    for document in collection.find(query, projection):
+        id_str_dict[document['id_str']] = document['in_reply_to_user_id_str']
 
     return id_str_dict
 
 def store_data_in_new_collection(db, data):
     # Create a new collection
-    new_collection = db['id_str_count_test']
+    new_collection = db['id_str_in_reply_to_user_id_str']
     # Convert the dictionary to a list of documents
-    documents = [{'id_str': key, 'count': value} for key, value in data.items()]
+    documents = [{'id_str': key, 'in_reply_to_user_id_str': value} for key, value in data.items()]
     # Insert the documents into the new collection
     new_collection.insert_many(documents)
 
 def main():
     db, collection = connect_to_db()
-    create_index_on_id_str(collection)
     id_str_dict = create_id_str_dict(collection)
     store_data_in_new_collection(db, id_str_dict)
-    print("Data stored in new collection 'id_str_count'.")
+    print("Data stored in new collection 'id_str_in_reply_to_user_id_str'.")
 
 if __name__ == "__main__":
     main()
