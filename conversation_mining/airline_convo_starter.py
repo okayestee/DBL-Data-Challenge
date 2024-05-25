abe1fa42421f4928f723a334676b874a57631129
esteerutten <e.rutten@student.tue.nl>
2024-05-25 16:40:20 +0200

    fix index & ids

--- conversation_mining/airline_convo_starter.py
+++ conversation_mining/airline_convo_starter.py
@@ -6,7 +6,7 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['DBL2']  # your database 
 removed_dupl = db['removed_duplicates']  # collection where duplicates are removed
 
-# List of airline IDs
+# List of airline IDs, make indexes
 airline_ids = ['56377143', '106062176', '18332190', '22536055', '124476322', 
                '26223583', '2182373406', '38676903', '1542862735', '253340062', 
                '45621423', '20626359', '218730857']
