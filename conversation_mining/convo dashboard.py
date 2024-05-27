0607abc8553d41fb909e26f5ca6226bf7ec20796
AnLucassen <a.e.p.p.lucassen@student.tue.nl>
2024-05-27 18:01:27 +0200

    dashboard

---
+++ conversation_mining/convo dashboard.py
@@ -0,0 +1,9 @@
+import airline_convo_starter
+
+
+#then all these data folder must be at same level
+data_folder_name: str = 'conversation' 
+data_convo_mining_folder_name: str = 'convo_mining'
+path: str = f'{data_convo_mining_folder_name}/../{data_folder_name}'
+airline_convo_starter.clean_all_files(path)
+
