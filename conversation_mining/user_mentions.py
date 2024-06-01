57f139740994e3d4ee635d86e1ffe325981717b0
esteerutten <e.rutten@student.tue.nl>
2024-06-01 19:22:20 +0200

    user_mention correction

--- conversation_mining/user_mentions.py
+++ conversation_mining/user_mentions.py
@@ -2,8 +2,8 @@ from pymongo import MongoClient
 
 # Connect to MongoDB
 client = MongoClient('mongodb://localhost:27017/')
-db = client['DBL2']
-tweets_collection = db['user_roots2']
+db = client['AirplaneMode']
+tweets_collection = db['tweets']
 
 # Function to filter tweets that mention a specific @user and return a list of those tweets
 def filter_tweets_by_mention(collection, user_handle):
@@ -14,12 +14,12 @@ def filter_tweets_by_mention(collection, user_handle):
     return mentions
 
 def main():
-    user_handle = '@americanair'  # Change this to the user you want to filter on
+    user_handle = '@americanair'  # Change this to any handle you want to filter by
     mentions = filter_tweets_by_mention(tweets_collection, user_handle)
     
     # Print the filtered tweets
     for mention in mentions:
-        print(f"- {mention['text']}")
+        print(f"- {mention}")
     
     return mentions
 
@@ -27,5 +27,6 @@ if __name__ == "__main__":
     mentions = main()
 
 client.close()
+
 
 
