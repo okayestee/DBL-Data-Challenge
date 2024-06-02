85b6c420601408b1b4fd312a700c12225f1995d5
20234278 <m.p.joosen@student.tue.nl>
2024-06-02 11:19:06 +0200

    Edited the cleaning dashboard to do all the cleaning steps

--- data cleaning/Cleaning dashboard.py
+++ data cleaning/Cleaning dashboard.py
@@ -1,13 +1,17 @@
 import data_cleans
+from conversation_mining import remove_duplicates, remove_inconsistencies
+import make_collection
+import pymongo
+
 
 # Make sure that all of your data is in one data folder
 # The data folder must be at the same level (have same parent folder) as the folder this dashboard is in 
 
 # Fill in the name of your data folder here:
-data_folder_name: str = 'data airlines'
+data_folder_name: str = 'data'
 
 # Fill in the name of the parent folder of this file
-data_cleaning_folder_name: str = 'data airlines' # 'data cleaning' by default
+data_cleaning_folder_name: str = 'data cleaning' # 'data cleaning' by default
 
 # Once folder_name contains the correct name
 # Run this file (Will take a while)
@@ -15,6 +19,16 @@ data_cleaning_folder_name: str = 'data airlines' # 'data cleaning' by default
 # The new file with the cleaned data will appear in the same folder as the data as 'airline_data'
 
 
+# Fill in the name of you database in mongoDB
+db = 'Airline_data'
+
 # DO NOT TOUCH THIS
-path: str = f'{data_cleaning_folder_name}/../{data_folder_name}'
+path: str = f'DBL-Data-Challenge/{data_folder_name}'
 data_cleans.clean_all_files(path)
+
+
+#make a collection in mongoDB
+make_collection(path, db)
+
+remove_duplicates.remove_duplicates(db)
+remove_inconsistencies.remove_inconsistencies(db)
