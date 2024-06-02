9a896f1df575a682a58d5bf901fedf53677ec86b
20234278 <m.p.joosen@student.tue.nl>
2024-06-02 21:15:47 +0200

    Made a lot of changes to topic analysis stuff

--- Business_idea/Topic_analysis.py
+++ Business_idea/Topic_analysis.py
@@ -1,38 +1,31 @@
 import pymongo
 from Utility_functions import *
-from Sentiment_analysis import VADER_implementation
-import BERTopic
+from bertopic import BERTopic
+from tqdm import tqdm
 
 topic_model = BERTopic.load("random_bertopic_model")
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client.Airline_data
-collection = db.removed_duplicates
+collection = db.no_inconsistency
 
 batch_mean_compound: list[float] = []
 
 if 'topic_analysis' not in db.list_collection_names():
-    new_collection = db.create_collection("sentiment_analysis")
+    new_collection = db.create_collection("topic_analysis")
 else:
     new_collection = db['topic_analysis']
 
 
-batch_size = 10000
+batch_size = 100
 documents_processed = 0
 
-trunc_error_counter = 0
-
-while True:
-    # Retrieve a batch of documents
-    batch = list(collection.find({}).skip(documents_processed).limit(batch_size))
-    if not batch:
-        print('Topic Analysis Finished!')
-        break  # Exit loop if no more documents are retrieved
+total_tweets = collection.count_documents({})
+with tqdm(total=total_tweets, desc="Topic analysis", unit="documents") as pbar:
+    for document in collection.find({}):
     
 
-
-    # Analyze sentiment for each document in the batch
-    for document in batch:
+    
         text = clean(get_full_text(document))
 
 
@@ -41,17 +34,15 @@ while True:
 
         # Get the topic and its name
         topic_number = topics[0]
-        topic = topic_model.get_topic(topic_number)
+        topic_name = topic_model.custom_labels_[topic_number]
+
+        document['topic'] = topic_name
+
+        new_collection.insert_one(document)
+
+        # Update the count of processed documents
+        pbar.update(1)
 
 
+ 
 
-        VADER_implementation.add_entire_document(document, new_collection)
-
-
-        new_collection.update_one()
-
-        VADER_implementation.add_to_document(document['id'], 'Topic', topic, new_collection)
-
-    # Update the count of processed documents
-    documents_processed += len(batch)
-    print(documents_processed)
