75615518887fabe17963c06ecb2f59634609a423
esteerutten <e.rutten@student.tue.nl>
2024-06-03 12:17:43 +0200

    deleting files

--- conversation_mining/convo dashboard.py
+++ conversation_mining/convo dashboard.py
@@ -8,6 +8,8 @@ from remove_inconsistencies import remove_inconsistencies
 from filter_replies import filter_replies
 from user_starting_convo import user_convo_starters
 from airline_starting_convo import airline_convo_starters
+from airline_tree
+
 
 def main():
     remove_duplicates()  # Call the function to remove duplicates
@@ -15,6 +17,7 @@ def main():
     filter_replies() # Call the function to store all the reply tweets in a new collection
     user_convo_starters() # Call the function to store all user starter tweets in a new collection
     airline_convo_starters()  # Call the function to store all airline starter tweets in a new collection
+    airline_trees()
 
 if __name__ == "__main__":
     main()
