9d7017cfebefec982c56b2ed722827640ab3381f
20234278 <m.p.joosen@student.tue.nl>
2024-06-12 09:21:30 +0200

    Added new_labels for sharing

--- Business_idea/Utility_functions.py
+++ Business_idea/Utility_functions.py
@@ -88,3 +88,11 @@ def filter_tweets_by_mention(collection, user_handle):
         if user_handle in tweet.get('text', ''):
             mentions.append(tweet)
     return mentions
+
+
+def new_labels(label, topic_labels):
+    label = get_real_label(label, topic_labels)
+    index = topic_labels.index(label)
+    new_labels = ['Undefined','Response', 'Qantas', 'Posted', 'Ryanair', 'Baggage', 'Easyjet', 'Avgeek', 'Lufthansa', 'Seats', 'Delays', 'Racial Retweet', 'Coronavirus','Cancelled','Booking reference', 'Airline','Refunds','KLM','Thank you for Flight','Roundtrip','Retweet backless seats', 'Twitter jargon','Retweet pride parade','Food','Retweet Breastfeeding','Retweet Lesbian','Climate','Retweet Lufthansa Iran','General Retweet','Corporate affairs','Racism','Locating baggage','Racial Retweet','Refunds','Retweet muslims','Italy','Retweet non-airline incident','Places in england','Retweet BTS','Flying','Retweet discrimination corona','Retweet lesbian sexualization','Flight tracker','Workers and Catering','Claims','Retweet Nigerian lawyer','Retweet Terrorism','Europa AirFrance']
+
+    return new_labels[index]
