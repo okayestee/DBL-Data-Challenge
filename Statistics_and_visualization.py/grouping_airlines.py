426afff5a626299931abc0e846e08fd48477e165
esteerutten <e.rutten@student.tue.nl>
2024-06-12 12:31:20 +0200

    fixed some issuees

--- Statistics_and_visualization.py/grouping_airlines.py
+++ Statistics_and_visualization.py/grouping_airlines.py
@@ -1,16 +1,51 @@
-#group the airline trees (username)
-#the user ones by mentioning an airline in their text
-
 from pymongo import MongoClient
+import re
+client = MongoClient("mongodb://localhost:27017/")
 
-# Connect to your MongoDB server
-client = MongoClient('mongodb://localhost:27017/')
-
-# Replace 'your_database' with the name of your database
 db = client['grouping_airlines']
+
+#list of airline collections
+
+airlines = [
+    "KLM", "AirFrance", "British_Airways", "AmericanAir", "Lufthansa", 
+    "AirBerlin", "easyJet", "RyanAir", "SingaporeAir", "Qantas", 
+    "EtihadAirways", "VirginAtlantic"]
+
+for airline in airlines:
+    db.create_collection(airline)
 
 # Replace 'your_collection' with the name of your original collection
 original_collection = db['airline_validtrees']
+
+# Mapping to handle different representations of the same airline
+airline_mapping = {
+    "KLM": "KLM",
+    "AirFrance": "AirFrance",
+    "British_Airways": "British_Airways",
+    "AmericanAir": "AmericanAir",
+    "Lufthansa": "Lufthansa",
+    "AirBerlin": "AirBerlin",
+    "easyJet": "easyJet",
+    "RyanAir": "RyanAir",
+    "SingaporeAir": "SingaporeAir",
+    "Qantas": "Qantas",
+    "EtihadAirways": "EtihadAirways",
+    "VirginAtlantic": "VirginAtlantic",
+    "British Airways": "British_Airways",
+    "Etihad Airways": "EtihadAirways",
+    "Royal Dutch Airlines": "KLM",
+    "Ryanair": "RyanAir",
+    "Singapore Airlines": "SingaporeAir",
+    "Virgin Atlantic": "VirginAtlantic"
+}
+
+# Function to find the correct collection name based on user_name
+def get_collection_name(user_name):
+    user_name = user_name.replace(" ", "").replace("-", "").lower()
+    for key, value in airline_mapping.items():
+        if re.sub(r'\W', '', key).lower() in user_name:
+            return value
+    return None
 
 # Fetch all documents from the original collection
 documents = original_collection.find()
@@ -21,11 +56,11 @@ for document in documents:
     user_name = document.get('user_name')
     
     if user_name:
-        # Define the target collection name based on user_name
-        target_collection_name = user_name
+        # Get the correct collection name based on user_name
+        target_collection_name = get_collection_name(user_name)
         
-        # Insert the document into the target collection
-        db[target_collection_name].insert_one(document)
+        if target_collection_name:
+            # Insert the document into the target collection
+            db[target_collection_name].insert_one(document)
 
 print("Documents have been copied to respective collections.")
-
