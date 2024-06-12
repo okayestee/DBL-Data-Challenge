ea661f514010086dbd2d0af2b633c45b192ca619
20234278 <m.p.joosen@student.tue.nl>
2024-06-12 16:31:43 +0200

    Finished topic adding function

--- Business_idea/Topic_share.py
+++ Business_idea/Topic_share.py
@@ -35,36 +35,38 @@ def add_topics(db_name, collection_name):
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client[db_name]
     collection = db[collection_name]
+    new_collection = db.topics
+
+
     total_tweets = collection.count_documents({})
-    with open('topic_share.json') as file:
+
+
+    with open('topic_share.json', 'r') as file:
+        file_gen = file.readlines()
         with tqdm(total= total_tweets, desc="Writing topics to collection", unit="documents") as pbar:
-            json_data = json.loads(file)
-            for tweet in json_data:
-                id_str = tweet['id_str']
-                topic = tweet['topic']
-        
-                result = collection.update_one(
-                {'id_str': id_str},
-                {'$set': {'topic': topic}})
 
-                if result.modified_count > 0:
-                    pbar.update(1)
-                else:
-                    print(f"No document found with id_str: {id_str} or no update was needed.")
-                    pbar.update(1)
+            for doc in collection.find({}):
+                for line in file_gen:
+
+                    data = json.loads(line)
+
+                    if str(data['id_str']) == str(doc['id_str']):
+                        doc['topic'] = data['topic']
+                        break
+
+                new_collection.insert_one(doc)
+                pbar.update(1)
 
 
 
-def tweets_without_topic(db_name, collection_name):
+def tweets_without_topic(db_name):
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client[db_name]
-    collection = db[collection_name]
+    collection = db['topics']
     topic_model = BERTopic.load('merged_model')
     old_labels = topic_model.custom_labels_
     documents_without_topic = collection.find({'topic': {'$exists': False}})
-    length = len(documents_without_topic)
-    print(length)
-    with tqdm(total= length, desc="Correcting tweets without topic", unit="documents") as pbar:
+    with tqdm(desc="Correcting tweets without topic", unit="documents") as pbar:
         for doc in documents_without_topic:
             topic = get_topic(get_full_text(doc), old_labels)
             id_str = doc['id_str']
--- Business_idea/Topic_share_dashboard.py
+++ Business_idea/Topic_share_dashboard.py
@@ -2,6 +2,5 @@ from Topic_share import *
 
 db = 'Airline_data'
 collection = 'no_inconsistency'
-
-add_topics(db, collection)
-tweets_without_topic(db, collection)
+add_topics(db,collection)
+tweets_without_topic(db)
