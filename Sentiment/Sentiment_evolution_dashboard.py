4bcb664c9957706f29ea0a33ecfa602637d0403e
20232815 <n.b.pillai@student.tue.nl>
2024-06-18 14:15:40 +0200

    Edited sentiment evolution

--- Sentiment/Sentiment_evolution_dashboard.py
+++ Sentiment/Sentiment_evolution_dashboard.py
@@ -4,11 +4,7 @@ import pymongo
 # Connect to the database
 client = pymongo.MongoClient("mongodb://localhost:27017/") ## Connect to MongoDB
 db = client['DBL'] ## Use the DBL database
-<<<<<<< HEAD
-collection = db['Airline_convos'] ## Choose a collection of conversations
-=======
 collection = db['valid_trees_airline_X'] ## Choose a collection of conversations
->>>>>>> 0657ac9c82b7320df106f6683b9e9043df2b44ab
 
 # Get the evolution statistics
 evolution_statistics = senti_evo.get_evolution_stats(senti_evo.get_tree_docs(collection))
