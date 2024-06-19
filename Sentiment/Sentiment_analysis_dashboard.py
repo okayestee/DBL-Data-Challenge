96a9e712f958343f53e6802f1699586a25c04fb3
20232815 <n.b.pillai@student.tue.nl>
2024-06-19 21:39:37 +0200

    Uncommented line 11 and commented out line 14-20

--- Sentiment/Sentiment_analysis_dashboard.py
+++ Sentiment/Sentiment_analysis_dashboard.py
@@ -8,13 +8,13 @@ db = client['DBL'] ## Use the DBL database
 collection = db['no_inconsistency'] ## Choose a collection of tweets
 
 # Create a new collection that includes all the same tweets but with the sentiment variables included
-#vader_imp.add_sentiment_variables(db, collection, 'Sentiment_included')
+vader_imp.add_sentiment_variables(db, collection, 'Sentiment_included')
 
 # Get the statistics and print them to the terminal
-sentiment_stats = senti_stats.get_sentiment_stats(collection)
+# sentiment_stats = senti_stats.get_sentiment_stats(collection)
 
-print(sentiment_stats)
+# print(sentiment_stats)
 
-senti_stats.plot_sentiment_stats(sentiment_stats)
+# senti_stats.plot_sentiment_stats(sentiment_stats)
 
-senti_stats.plot_means(sentiment_stats) # Does not give correct means for some reason :(
+# senti_stats.plot_means(sentiment_stats) # Does not give correct means for some reason :(
